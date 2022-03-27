ca01abfef202334eba6c8da99750fe31a139b17f
Jason Smith <Jason@zapleaf.com>
2022-03-27 01:41:21 -0400

    Setting up DbContext

--- Program.cs
+++ Program.cs
@@ -1,7 +1,23 @@
+using EpicAcre.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Tell application to use DbContext, using AppDbContext class file,
+// and DefaultConnection string defined in appsettings (passed as options)
+// install EntityFrameworkCore.SqlServer
+// GetConnectionStrings looks in the ConnectionsStrings block of the appsettings json file
+// Add EntityFrameworkCore.Tools NuGet to use Migrations in PM Console
+//    1.) "add-migrations <name>" creates the Migrations folder and migration class
+//    2.) "update-database" updates database
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
+// added so when view is updated it updates in brwoser without restarting applicaiton
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
